a6ae8319065ca2906e88ca8ec2ab3a14e31061bb
Laptop <alex.garzamajora@gmail.com>
2025-01-26 20:35:54 -0600

    new file:   Semana1/Actividad3.py

---
+++ Semana1/Actividad3.py
@@ -0,0 +1,34 @@
+#Ejercicio 1: ¿Cual es la funcion de la memoria secundaria de una computadora?
+#c) Almacenar datos durante mucho tiempo, incluso despues de ciclos de apagado y encendido
+
+#Ejercicio 2: ¿Que es un programa?
+#Es un conjunto de instruciones que se tienen que seguir y ejecutar para realizar una o varias tareas en especifico
+
+#Ejercicio 3: ¿Cual es la diferencia entre un compilador y un interprete?
+#Un compilador traduce todo el lenguaje de programacion (codigo fuente) a el lenguaje maquina mientras que un interprete solo lee y ejecuta el codigo fuente linea por linea
+
+#Ejercicio 4: ¿Cual de los siguientes tiene codigo maquina?
+#Al ser python un lenguaje interpretado, ninguna de las opciones tiene lenguaje de maquina al menos que el programa sea compilado y estemos ablando del ejecutable
+
+#Ejercicio 5: ¿Que esta mal en el siguiente codigo?
+#La instruccion print tiene una sintaxis erronea y un error de ortografia. La forma correcta es
+#print("Hola Mundo!")
+
+#Ejercicio 6: ¿En que lugar del computador queda almacenada una variable despues de ser definida en python?
+#b) Memoria principal
+
+#Ejercicio 7: ¿Que mostrara en la pantalla el siguiente programa?
+          #x=43
+          #x=x+1
+          #print(x)
+#b) 44
+
+#Ejercicio 8: Explique cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana
+#(1)Unidad central de procesamiento: El cerebro humano
+#(2)Memoria pricipal: Memoria a corto plazo
+#(3)Memoria secundaria: Memoria a largo plazo
+#(4)Dispositivo de entrada: Los sentidos humanos
+#(5)Dispositivos de salida: Habilidades como el habla o la locomocion.
+
+#Ejercicio 9: ¿Como puedes corregir un error de sintaxis?
+#Revisando la instruccion mal escrita y corrigiendo su ortografia, parametros o atributos 
